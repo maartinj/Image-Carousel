9dcf236b06ee97a430a7a4e5720132c4e50aa2a3
Marcin Jędrzejak <mjedrzejak@icloud.com>
2024-08-05 01:31:19 +0200

    feat: Enhanced ScrollView Carousel (completed Image Carousel in SwiftUI)

--- ImageCarousel/Views/ScrollViewCarouselView2.swift
+++ ImageCarousel/Views/ScrollViewCarouselView2.swift
@@ -14,10 +14,45 @@ import SwiftUI
 
 struct ScrollViewCarouselView2: View {
     @Environment(Store.self) private var store
+    @State private var scrollID: Int?
     var body: some View {
         NavigationStack {
-            Text("ScrollView Carousel +")
-                .navigationTitle("ScrollView +")
+            VStack {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: 0) {
+                        ForEach(0..<store.sampleImages.count, id: \.self) { index in
+                            let sampleImage = store.sampleImages[index]
+                            VStack {
+                                AsyncImage(url: sampleImage.imageUrl) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(maxWidth: .infinity)
+                                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                                        .shadow(radius: 10)
+                                        .padding()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                Text(sampleImage.caption)
+                                    .font(.title)
+                            }
+                            .containerRelativeFrame(.horizontal)
+                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
+                                content
+                                    .opacity(phase.isIdentity ? 1.0 : 0.6)
+                                    .scaleEffect(phase.isIdentity ? 1.0 : 0.6)
+                            }
+                        }
+                    }
+                    .scrollTargetLayout()
+                }
+                .scrollPosition(id: $scrollID)
+                .scrollTargetBehavior(.paging)
+                IndicatorView(imageCount: store.sampleImages.count, scrollID: $scrollID)
+                // Indicator bar here
+            }
+            .navigationTitle("ScrollView")
         }
     }
 }
@@ -26,3 +61,26 @@ struct ScrollViewCarouselView2: View {
         .environment(Store())
 }
 
+struct IndicatorView: View {
+    let imageCount: Int
+    @Binding var scrollID: Int?
+    
+    var body: some View {
+        HStack {
+            ForEach(0..<imageCount, id: \.self) { indicator in
+                let index = scrollID ?? 0
+                Button {
+                    withAnimation {
+                        scrollID = indicator
+                    }
+                } label: {
+                    Image(systemName: "circle.fill")
+                        .font(.system(size: 8))
+                        .foregroundStyle(indicator == index ? Color.white : Color(.lightGray))
+                }
+            }
+        }
+        .padding(7)
+        .background(RoundedRectangle(cornerRadius: 10).fill(Color(.lightGray)).opacity(0.2))
+    }
+}
